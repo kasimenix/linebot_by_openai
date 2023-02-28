fd55b731095f0d609bb179a494b6d739eac7839b
an <ksaimenix@gmail.com>
2023-02-28 21:30:14 +0800

    更新token內容

--- index.py
+++ index.py
@@ -3,21 +3,16 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from api.chatgpt import ChatGPT
-
 import os
-
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
-
 app = Flask(__name__)
 chatgpt = ChatGPT()
-
 # domain root
 @app.route('/')
 def home():
     return 'Hello, World!'
-
 @app.route("/webhook", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -31,29 +26,13 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
-
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     global working_status
     
     if event.message.type != "text":
         return
-    
-    if event.message.text == "啟動":
-        working_status = True
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="我是時下流行的AI智能，目前可以為您服務囉，歡迎來跟我互動~"))
-        return
-
-    if event.message.text == "安靜":
-        working_status = False
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="感謝您的使用，若需要我的服務，請跟我說 「啟動」 謝謝~"))
-        return
-    
+    working_status = True
     if working_status:
         chatgpt.add_msg(f"Human:{event.message.text}?\n")
         reply_msg = chatgpt.get_response().replace("AI:", "", 1)
@@ -61,7 +40,5 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=reply_msg))
-
-
 if __name__ == "__main__":
     app.run()
